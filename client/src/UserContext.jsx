405112009ebd49ed3e6e883da7a9666263b76267
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-03-17 15:53:36 -0500

    update account page, created logout function, start on places form

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -4,16 +4,18 @@ import axios from "axios";
 export const UserContext = createContext({});
 export function UserContextProvider({children}) {
     const [user, setUser] = useState(null); 
+    const [ready, setReady] = useState(false);
     useEffect(() => {
         //if user is empty try to get user info
         if(!user){
             axios.get('/profile').then(({data}) => {
-                setUser(data);         
+                setUser(data); 
+                setReady(true);        
             });
         }
     }, []);
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser, ready}}>
             {children}
         </UserContext.Provider>
     );
