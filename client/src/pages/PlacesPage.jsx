159f92b605d31a1c1ceec6c82ba151e7a70b3cb7
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-03-19 12:41:47 -0500

    updates to upload photo by link, displays photos uploaded by link

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -85,7 +85,7 @@ export default function PlacesPage(){
                         <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                             {addedPhotos.length > 0 && addedPhotos.map(link => (
                                 <div>
-                                    <img className="rounded-2xl" src={'http://localhost:4000/uploads/' + link} alt="" />
+                                    {<img className="rounded-2xl" src={'http://localhost:4000/uploads/' + link} alt="" />}
                                 </div>
                             ))}
                             <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
