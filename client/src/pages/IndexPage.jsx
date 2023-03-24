6169a3f120bdc77eb27c9d869294db707155e0b7
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-03-24 15:34:21 -0500

    single place page completed

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
+
 
 export default function IndexPage() {
   const [places, setPlaces] = useState([]);
@@ -10,8 +12,8 @@ export default function IndexPage() {
   }, []);
   return (
         <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-           {places.length > 0 && places.map(place => 
-              <div>
+           {places.length > 0 && places.map(place => (
+              <Link to={'/place/' + place._id}>
                 <div className="bg-gray-500 mb-2 rounded-2xl flex">
                   {place.photos?.[0] && (
                     <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt="" />
@@ -22,8 +24,9 @@ export default function IndexPage() {
                 <div className="mt-1">
                   <span className="font-bold">${place.price}</span> per night
                 </div>
-              </div>
+              </Link>
               )
+            )
            }
         </div>
       );
