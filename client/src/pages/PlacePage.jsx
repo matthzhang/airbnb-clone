0d766d0cd7ec5077b8445a066e999b7a6b9ec7da
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-03-25 12:22:22 -0500

    additional formatting changes on single page display

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -42,7 +42,7 @@ export default function PlacePage() {
     }
 
     return (
-        <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
+        <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8 items center">
             <h1 className="text-3xl">{place.title}</h1>
             <a className="flex gap-1 my-3 block font-semibold underline" target="_blank" href={'https://maps.google.com/?q=' + place.address}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -52,31 +52,43 @@ export default function PlacePage() {
                 {place.address}
             </a>
             <div className="relative">
-                <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-                    <div>
-                        {place.photos?.[0] && (
-                            <div>
-                                <img onClick={() => setShowAllPhotos(true)} className="cursor-pointer aspect-square object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} />
-                            </div>
-                        )}
-                    </div>
-                    <div className="grid">
-                        {place.photos?.[1] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="cursor-pointer aspect-square object-cover" src={'http://localhost:4000/uploads/' + place.photos[1]} />
-                        )}
+                <div className="flex">
+                    {place.photos?.[0] && (
+                        <div>
+                            <img onClick={() => setShowAllPhotos(true)} className="h-full rounded-3xl overflow-hidden object-cover cursor-pointer" src={'http://localhost:4000/uploads/' + place.photos[0]} />
+                        </div>
+                    )}
+                    <div className="px-2 grid flex rounded-3xl overflow-hidden">
+                        <div>
+                            {place.photos?.[1] && (
+                                <img onClick={() => setShowAllPhotos(true)} className="h-full aspect-square object-cover cursor-pointer" src={'http://localhost:4000/uploads/' + place.photos[1]} />
+                            )}
+                        </div>
                         <div className="overflow-hidden">
                             {place.photos?.[2] && (
-                                <img onClick={() => setShowAllPhotos(true)} className="cursor-pointer aspect-square object-cover relative top-2" src={'http://localhost:4000/uploads/' + place.photos[2]} />
+                                <img onClick={() => setShowAllPhotos(true)} className="h-full aspect-square object-cover relative top-2 cursor-pointer" src={'http://localhost:4000/uploads/' + place.photos[2]} />
                             )}
                         </div>
                     </div>
+                    <div className="grid flex rounded-3xl overflow-hidden relative">
+                        <div>
+                            {place.photos?.[3] && (
+                                <img onClick={() => setShowAllPhotos(true)} className="h-full aspect-square object-cover cursor-pointer" src={'http://localhost:4000/uploads/' + place.photos[3]} />
+                            )}
+                        </div>
+                        <div className="overflow-hidden">
+                            {place.photos?.[4] && (
+                                <img onClick={() => setShowAllPhotos(true)} className="h-full aspect-square object-cover relative top-2 cursor-pointer" src={'http://localhost:4000/uploads/' + place.photos[4]} />
+                            )}
+                        </div>
+                        <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            Show more photos
+                        </button>
+                    </div>
                 </div>
-                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                    </svg>
-                    Show more photos
-                </button>
             </div>
             <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_2fr]">
                 <div>
